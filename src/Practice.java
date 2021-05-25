a5f68de8106e5df9929a3875e6686f27bb1c72f0
Darius Brown <Brown.darius.r@gmail.com>
2021-05-24 21:58:29 -0500

    merge

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Practice {
@@ -10,12 +12,12 @@ public class Practice {
 //}
 // example hash map
 
-//    public ArrayList<String> merge(String[] words, String[] more) {
-//        ArrayList<String> sentence = new ArrayList<String>();
-//        for (String w : words) sentence.add(w);
-//        for (String w : more) sentence.add(w);
-//        return sentence;
-//    }
+    public static ArrayList<String> merge(String[] words, String[] more) {
+        ArrayList<String> sentence = new ArrayList<String>();
+        for (String w : words) sentence.add(w);
+        for (String w : more) sentence.add(w);
+        return sentence;
+    }
 
 // Implement an algorithm to determine if a string has all unique characters. What is you cannot use additional data structures.
 
@@ -25,7 +27,9 @@ public class Practice {
 
 
     public static void main(String[] args) {
-
+        String[] newArr = {"fun", "dun", "bun"};
+        String[] addArr = {"fan", "dan", "ban"};
+        System.out.println(merge(newArr,addArr));
 
     }
 };
